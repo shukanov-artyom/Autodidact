199716fe06a5bf09139670704cb3c5af359fede5
Artyom Shukanov <shukanov.artyom@gmail.com>
2017-11-20 17:43:46 +0300

    Link extractor development

---
+++ Bot/Utils/LinkExtractor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Bot.Utils
+{
+    public class LinkExtractor
+    {
+        private readonly string text;
+
+        public LinkExtractor(string text)
+        {
+            this.text = text;
+        }
+
+        public string ExtractLink()
+        {
+            
+        }
+    }
+}
